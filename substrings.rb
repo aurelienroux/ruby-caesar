e5aca972a2f1882a77bbadabc6570b446bdb9b16
Aurelien Roux <aureroux13@gmail.com>
2025-12-02 10:50:50 +0100

    clean up hash assignation

--- substrings.rb
+++ substrings.rb
@@ -1,23 +1,19 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substrings (sentence, dictionnary)
-  hash = Hash.new()
+  result = Hash.new()
 
   sentence.scan(/[a-zA-Z]+/) { |word|
     dictionnary.each do |w|
       if word.downcase.include?(w)
-        if hash[w]
-          hash[w] = hash[w] + 1
-        else
-          hash[w] = 1
-        end
+        result[w] =+ 1
       end
     end
   }
 
- puts hash
+  return result
 end
 
-# substrings("below", dictionary)
-# substrings("own", dictionary)
-substrings("Howdy partner, sit down! How's it going?", dictionary)
+# puts substrings("below", dictionary)
+# puts substrings("own", dictionary)
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
