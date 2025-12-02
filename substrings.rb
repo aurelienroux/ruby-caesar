b5978cf7636d29cb1a5a02cb6508f82e47d418b0
Aurelien Roux <aureroux13@gmail.com>
2025-12-02 19:04:25 +0100

    add a whattodo on substrings

--- substrings.rb
+++ substrings.rb
@@ -1,3 +1,8 @@
+# Implement a method #substrings that takes a word as the first argument and
+# then an array of valid substrings (your dictionary) as the second argument.
+# It should return a hash listing each substring (case insensitive) that was
+# found in the original string and how many times it was found.
+
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substrings (sentence, dictionnary)
@@ -16,4 +21,4 @@ end
 
 # puts substrings("below", dictionary)
 # puts substrings("own", dictionary)
-puts substrings("Howdy partner, sit down! How's it going?", dictionary)
+# puts substrings("Howdy partner, sit down! How's it going?", dictionary)
